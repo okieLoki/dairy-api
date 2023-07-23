e3f733c5318c973031c23455adc515bf84e6c769
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-07-23 18:03:23 +0530

    cors setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const dbConnect = require('./config/db')
+const cors = require('cors')
 const authUserRoute = require('./routes/authUserRoute')
 const rateListRoute = require('./routes/rateListRoute')
 const farmerRoute = require('./routes/farmerRoute')
@@ -12,6 +13,7 @@ dbConnect()
 
 const server = express()
 server.use(express.json())
+server.use(cors())
 
 server.use('/api/user', authUserRoute)
 server.use('/api/ratelist', rateListRoute)
