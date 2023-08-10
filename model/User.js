da24d2f3e9b35e3dc1c82e4f435528b6e0ee72d0
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-08-10 11:16:38 +0530

    unique key removed from userId

--- model/User.js
+++ model/User.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     userId: {
         type: Number,
-        unique: true,
         required: [true, 'User ID missing'],
     },
     username: {
