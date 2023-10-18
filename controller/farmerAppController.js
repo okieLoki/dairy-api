e50cbf14890df26bf951ea8c641c081e89204ba5
loki <uddeeptaraajkashyap@gmail.com>
2023-10-18 15:52:02 +0530

    update: farmer controllers added

--- controller/farmerAppController.js
+++ controller/farmerAppController.js
@@ -3,6 +3,7 @@ const handleErrors = require('../utils/handleErrors')
 const { sendOTP, generateOTP } = require('../service/otpService')
 const jwt = require('jsonwebtoken')
 const Farmer = require('../model/Farmer')
+const Collection = require('../model/Collection')
 
 // Controller functions for farmer app
 const reqOTPFarmer = async (req, res) => {
@@ -66,25 +67,167 @@ const verifyOTPandLogin = async (req, res) => {
 
         if (farmer.otp_expiry < new Date()) throw createError.BadRequest('OTP expired');
 
+        farmer.verified = true;
 
-        const token = jwt.sign({
-            farmer_id: farmer._id,
-            farmer_name: farmer.farmerName,
-            farmerID: farmer.farmerId,
-            mobile_number: farmer.mobileNumber,
-            user_id: farmer.userId,
-            verified: farmer.verified
-        },
-            process.env.SECRET_KEY,
-            {
-                expiresIn: '20h'
+        const result = await farmer.save();
+
+        if (result) {
+
+            const token = jwt.sign({
+                farmer_id: farmer._id,
+                farmer_name: farmer.farmerName,
+                farmerID: farmer.farmerId,
+                mobile_number: farmer.mobileNumber,
+                user_id: farmer.userId,
+                verified: farmer.verified
+            },
+                process.env.SECRET_KEY,
+                {
+                    expiresIn: '20h'
+                });
+
+            return res.status(200).json({
+                message: 'OTP verified successfully',
+                token
             });
+        } else {
+            throw createError.InternalServerError('Internal Server Error');
+        }
+    } catch (error) {
+        handleErrors(error, res);
+    }
+};
+
+const getFarmerCollections = async (req, res) => {
+    try {
+
+        const farmer_id = req.farmer._id
+
+        // check if verified
+        const farmer = await Farmer.findById(farmer_id)
+        if (!farmer.verified) throw createError.BadRequest('Farmer not verified')
+
+        const collections = await Collection.find({
+            farmerId: farmer.farmerId,
+            userId: farmer.userId
+        })
+
+        if (!collections) throw createError.NotFound('No collections found')
 
         return res.status(200).json({
-            message: 'OTP verified successfully',
-            token
+            message: 'Collections fetched successfully',
+            collections
+        })
+
+
+    } catch (error) {
+        handleErrors(error, res)
+    }
+}
+
+const getCollectionsDaily = async (req, res) => {
+    try {
+        const farmer_id = req.farmer._id;
+        const farmer = await Farmer.findById(farmer_id);
+
+        if (!farmer.verified) {
+            throw createError.BadRequest('Farmer not verified');
+        }
+
+        const today = new Date();
+        today.setUTCHours(0, 0, 0, 0);
+
+        const nextDay = new Date(today);
+        nextDay.setDate(nextDay.getDate() + 1);
+
+        const collections = await Collection.find({
+            farmerId: farmer.farmerId,
+            userId: farmer.userId,
+            collectionDate: { $gte: today, $lt: nextDay }
         });
 
+        if (collections.length === 0) {
+            throw createError.NotFound('No collections found for today');
+        }
+
+        return res.status(200).json({
+            message: 'Collections fetched successfully',
+            collections
+        });
+    } catch (error) {
+        handleErrors(error, res);
+    }
+};
+
+const getCollectionsWeekly = async (req, res) => {
+    try {
+        const farmer_id = req.farmer._id;
+        const farmer = await Farmer.findById(farmer_id);
+
+        if (!farmer.verified) {
+            throw createError.BadRequest('Farmer not verified');
+        }
+
+        const today = new Date();
+        today.setUTCHours(0, 0, 0, 0);
+
+        const sevenDaysAgo = new Date(today);
+        sevenDaysAgo.setDate(today.getDate() - 6);
+        const endOfToday = new Date(today);
+        endOfToday.setUTCHours(23, 59, 59, 999);
+
+        const collections = await Collection.find({
+            farmerId: farmer.farmerId,
+            userId: farmer.userId,
+            collectionDate: { $gte: sevenDaysAgo, $lte: endOfToday }
+        });
+
+        if (collections.length === 0) {
+            throw createError.NotFound('No collections found for the past 7 days');
+        }
+
+        return res.status(200).json({
+            message: 'Collections fetched successfully',
+            collections
+        });
+    } catch (error) {
+        handleErrors(error, res);
+    }
+};
+
+
+
+
+const getCollectionsMonthly = async (req, res) => {
+    try {
+        const farmer_id = req.farmer._id;
+        const farmer = await Farmer.findById(farmer_id);
+
+        if (!farmer.verified) {
+            throw createError.BadRequest('Farmer not verified');
+        }
+
+        const today = new Date();
+        today.setUTCHours(0, 0, 0, 0);
+        today.setDate(1);
+
+        const nextMonth = new Date(today);
+        nextMonth.setUTCMonth(nextMonth.getUTCMonth() + 1);
+
+        const collections = await Collection.find({
+            farmerId: farmer.farmerId,
+            userId: farmer.userId,
+            collectionDate: { $gte: today, $lt: nextMonth }
+        });
+
+        if (collections.length === 0) {
+            throw createError.NotFound('No collections found for the current month');
+        }
+
+        return res.status(200).json({
+            message: 'Collections fetched successfully',
+            collections
+        });
     } catch (error) {
         handleErrors(error, res);
     }
@@ -93,5 +236,9 @@ const verifyOTPandLogin = async (req, res) => {
 
 module.exports = {
     reqOTPFarmer,
-    verifyOTPandLogin
+    verifyOTPandLogin,
+    getFarmerCollections,
+    getCollectionsDaily,
+    getCollectionsWeekly,
+    getCollectionsMonthly
 }
--- routes/farmerRoute.js
+++ routes/farmerRoute.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 const {authFarmer} = require('../middleware/auth')
 
-const {reqOTPFarmer, verifyOTPandLogin} = require('../controller/farmerAppController')
+const {
+    reqOTPFarmer, 
+    verifyOTPandLogin,
+    getFarmerCollections,
+    getCollectionsDaily,
+    getCollectionsWeekly,
+    getCollectionsMonthly
+} = require('../controller/farmerAppController')
 
 router.post('/otp', reqOTPFarmer)
 
 router.use(authFarmer)
 router.patch('/otp/verify', verifyOTPandLogin)
+router.get('/collections', getFarmerCollections)
+router.get('/collections/daily', getCollectionsDaily)
+router.get('/collections/weekly', getCollectionsWeekly)
+router.get('/collections/monthly', getCollectionsMonthly)
 
 module.exports = router;
