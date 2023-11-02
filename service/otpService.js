9f01c0ae97dcad9b5171dbf38efd64fd84daa717
loki <uddeeptaraajkashyap@gmail.com>
2023-11-02 21:20:31 +0530

    update: otp

--- service/otpService.js
+++ service/otpService.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const generateOTP = () => {
-    
+
     const otp = Math.floor(1000 + Math.random() * 9000);
     let expiry = new Date();
 
@@ -17,6 +17,24 @@ const generateOTP = () => {
 
 const sendOTP = async (mobileNumber, otp) => {
     console.log('Sending OTP')
+
+    const url = 'https://sms.aakashsms.com/sms/v3/send';
+    const text = `Your OTP for Hamroo Farmer Login is ${otp}`
+    const token = process.env.SMS_TOKEN;
+
+    const formData = new URLSearchParams();
+    formData.append('token', token);
+    formData.append('to', mobileNumber);
+    formData.append('text', text);
+
+    axios.post(url, formData)
+        .then(response => {
+            console.log('Response:', response.data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
     return true
 }
 
